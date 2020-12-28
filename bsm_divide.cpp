ae9b20491fe75028369c617c5338f4ddd163db05
yuziroppe <syani4732@gmail.com>
2020-12-28 22:44:29 +0900

    n追加+tupleをstructに

--- bsm_divide.cpp
+++ bsm_divide.cpp
@@ -1,78 +1,68 @@
 #include <boost/multiprecision/cpp_int.hpp>
 #include <boost/multiprecision/cpp_dec_float.hpp>
 #include <iostream>
-#include <tuple>
 #include <boost/multiprecision/gmp.hpp>
 
 using namespace std;
 namespace mp = boost::multiprecision;
 
-const int N = 100000000;
+const int64_t N = 100000000;
+const int64_t n = N / 14;
 
 using BigFloat = mp::number<mp::gmp_float<N>>;
-
 using BigInt = mp::mpz_int;
 
-using XYZ = tuple<BigInt, BigInt, BigInt>;
-
 const BigInt A = 13591409, B = 545140134, C = 640320;
+const BigInt CT = C * C * C;
+const BigInt CTd24 = CT / 24;
+
+struct XYZ
+{
+	BigInt x, y, z;
+};
 
 // x_k = X(k, k+1)
-BigInt calc_x(int k)
+inline BigInt calc_x(int64_t k)
 {
 	if (k == 0)
-		return BigInt(1);
-	return mp::pow(BigInt(k), 3) * mp::pow(C, 3) / 24;
+		return 1;
+	return (k * k) * CTd24 * k;
 }
 
 // y_k = Y(k, k+1)
-BigInt calc_y(int k)
+inline BigInt calc_y(int64_t k)
 {
 	return A + B * k;
 }
 
 // z_k = Z(k, k+1)
-BigInt calc_z(int k)
+inline BigInt calc_z(int64_t k)
 {
-	return (-1) * (6 * BigInt(k) + 1) * (2 * BigInt(k) + 1) * (6 * BigInt(k) + 5);
+	if (k == n - 1)
+		return 0;
+	return (-1) * BigInt((6 * k + 1) * (2 * k + 1)) * (6 * k + 5);
 }
 
-XYZ calc(int l, int r)
+XYZ calc(int64_t l, int64_t r)
 {
 	if (r - l == 1)
 	{
-		return make_tuple(calc_x(l), calc_y(l), calc_z(l));
+		return {calc_x(l), calc_y(l), calc_z(l)};
 	}
 
-	int m = (l + r) >> 1;
+	int64_t m = (l + r) >> 1;
 
 	XYZ lc = calc(l, m);
 	XYZ rc = calc(m, r);
 
-	BigInt lx = get<0>(lc);
-	BigInt rx = get<0>(rc);
-	BigInt X = lx * rx;
-
-	BigInt lz = get<2>(lc);
-	BigInt rz = get<2>(rc);
-	BigInt Z = lz * rz;
-
-	BigInt ly = get<1>(lc);
-	BigInt ry = get<1>(rc);
-	BigInt Y = rx * ly + ry * lz;
-
-	return make_tuple(X, Y, Z);
+	return {lc.x * rc.x, rc.x * lc.y + rc.y * lc.z, lc.z * rc.z};
 }
 
 int main()
 {
+	XYZ xyz = calc(0, n);
+	BigFloat p = mp::sqrt(BigFloat(CT)) * xyz.x / 12 / xyz.y;
 
-	BigFloat tmp = mp::sqrt(BigFloat(C) * BigFloat(C) * BigFloat(C)) / 12;
-
-	XYZ xyz = calc(0, N);
-	BigInt X = get<0>(xyz);
-	BigInt Y = get<1>(xyz);
-
-	cout << setprecision(N) << tmp * X / Y << endl;
+	cout << setprecision(N) << p << endl;
 	return 0;
 }
